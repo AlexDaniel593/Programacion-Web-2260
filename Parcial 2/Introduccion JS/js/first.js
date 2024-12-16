6826dfa4ada4c3c302ec2c736fb9bd3264bfa918
AlexDaniel593 <dalexis203@gmail.com>
2024-12-16 11:06:55 -0500

    Add new HTML, CSS, and JavaScript files; remove outdated files

---
+++ Parcial 2/Introduccion JS/js/first.js
@@ -0,0 +1,10 @@
+var nombre = "Juan";
+var edad;
+edad = 30;
+var apellido = "Perez";
+console.log("Mi nombre es:" + nombre + " " + apellido + " " + edad);  
+
+let x=10;
+let y=20;
+let z=x+y;
+console.log(z);
